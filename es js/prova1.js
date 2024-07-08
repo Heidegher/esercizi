5c7eee7316c347b14acba75cf9b3e92f78e3362d
Heidegher <144094368+Heidegher@users.noreply.github.com>
2024-07-08 16:02:50 +0200

    Add files via upload

---
+++ es js/prova1.js
@@ -0,0 +1,43 @@
+let a = 12;
+let b = 20;
+
+console.log(a+b);
+
+const somma = 12+20;
+
+console.log(somma)
+
+///es4
+
+let x = 12;
+
+///es5
+
+let nome = "Riccardo"
+
+console.log(nome);
+
+///es6
+
+let sottrazione = 4 - x;
+
+console.log(sottrazione)
+
+///es7
+
+let nome1 = "john";
+let nome2 = "John";
+
+
+const disuguali = nome1 !== nome2;
+const uguaglianza = nome1 === nome2;
+let min = nome2.toLowerCase() === nome1;
+
+
+console.log(disuguali);
+console.log(uguaglianza);
+console.log(min);
+
+
+
+
